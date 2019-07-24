ec105260226c11ecff1e3b8d3006cf923e0e26de
dungv <dungv@cs.ucr.edu>
2019-07-24 09:07:33 -0700

    Update install.rb

--- onbase_web/recipes/install.rb
+++ onbase_web/recipes/install.rb
@@ -16,13 +16,17 @@ remote_file 'C:\tmp\setup.exe' do
   action :create
 end
 
-
-windows_package 'onbasesetup' do
-  source 'C:\tmp\setup.exe'
-  action :install
+execute 'setup onbase web' do
+  source 'setup.exe'
+  cwd 'C:\tmp'
 end
 
-windows_package 'onbaseweb' do
-  source 'C:\tmp\Hyland.Web.Server.17.msi'
-  action :install
-end
+#windows_package 'onbasesetup' do
+#  source 'C:\tmp\setup.exe'
+#  action :install
+#end
+
+#windows_package 'onbaseweb' do
+#  source 'C:\tmp\Hyland.Web.Server.17.msi'
+#  action :install
+#end
