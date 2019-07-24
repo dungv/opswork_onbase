5e357da9d66a774698641f0a5193c8db1c29815a
dungv <dungv@cs.ucr.edu>
2019-07-23 17:14:45 -0700

    Update install.rb

--- onbase_web/recipes/install.rb
+++ onbase_web/recipes/install.rb
@@ -1,23 +1,4 @@
-directory 'C:\tmp' do
-  rights :full_control, 'Everyone'
-  recursive true
-  action :create
-end
-
-cookbook_file 'C:\tmp\Hyland Web Server 17.msi' do
-  source "Hyland Web Server 17.msi"
-  rights :full_control, 'Everyone'
-  action :create
-end
-
-cookbook_file 'C:\tmp\setup.exe' do
-  source "setup.exe"
-  rights :full_control, 'Everyone'
-  action :create
-end
-
-
 windows_package 'onbaseweb' do
-  source 'C:\tmp\setup.exe'
+  source 'https://github.com/dungv/opswork_onbase/releases/download/onbaseweb/Hyland.Web.Server.17.msi'
   action :install
 end
