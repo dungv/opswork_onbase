c6fd5161d22e80b2b90aaba674cd48cbfc2c962d
dungv <dungv@cs.ucr.edu>
2019-07-25 17:48:59 -0700

    Update install.rb

--- onbase_app/install.rb
+++ onbase_app/install.rb
@@ -1,1 +1,16 @@
-d
+directory 'C:\tmp' do
+  rights :full_control, 'Everyone'
+  recursive true
+  action :create
+end
+
+remote_file 'C:\tmp\Hyland.Web.Server.17.msi' do
+  source 'https://github.com/dungv/opswork_onbase/releases/download/onbaseweb/Hyland.Web.Server.17.msi'
+  rights :full_control, 'Everyone'
+  action :create
+end
+
+windows_package 'onbaseapp' do
+  source 'C:\tmp\Hyland.Web.Server.17.msi'
+  action :install
+end
