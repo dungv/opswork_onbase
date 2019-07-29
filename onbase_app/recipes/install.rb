06d50188e8d7c724795f42ae59c25d970e325c24
dungv <dungv@cs.ucr.edu>
2019-07-29 11:25:35 -0700

    Update install.rb

--- onbase_app/recipes/install.rb
+++ onbase_app/recipes/install.rb
@@ -1,22 +1,22 @@
-directory 'C:\tmp' do
-  rights :full_control, 'Everyone'
-  recursive true
-  action :create
-end
-
-remote_file 'C:\tmp\Hyland.Application.Server.17.x86.msi' do
-  source 'https://github.com/dungv/opswork_onbase/releases/download/onbaseweb/Hyland.Application.Server.17.x86.msi'
-  rights :full_control, 'Everyone'
-  action :create_if_missing
-end
-
-#windows_package 'onbaseapp' do
-#  source 'C:\tmp\Hyland.Application.Server.17.x86.msi'
-#  installer_type :custom
-#  options '/Q'
+#directory 'C:\tmp' do
+#  rights :full_control, 'Everyone'
+#  recursive true
+#  action :create
 #end
 
-execute  'onbaseapp' do
-  command 'msiexec /qn /i "c:\tmp\hyland.application.server.17.x86.msi'
-  elevated :true
+#remote_file 'C:\tmp\Hyland.Application.Server.17.x86.msi' do
+#  source 'https://github.com/dungv/opswork_onbase/releases/download/onbaseweb/Hyland.Application.Server.17.x86.msi'
+#  rights :full_control, 'Everyone'
+#  action :create_if_missing
+#end
+
+windows_package 'onbaseapp' do
+  source 'https://github.com/dungv/opswork_onbase/releases/download/onbaseweb/Hyland.Application.Server.17.x86.msi'
+  installer_type :custom
+  options '/Q'
 end
+
+#execute  'onbaseapp' do
+#  command 'msiexec /qn /i "c:\tmp\hyland.application.server.17.x86.msi'
+#  elevated :true
+#end
